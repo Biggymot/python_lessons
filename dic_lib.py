dbb50241a152f46a6253e97001573b773d955f7a
Dmitriy Bogach <dm.bogach@gmail.com>
2016-06-18 21:26:02 +0300

    Initial commit

---
+++ dic_lib.py
@@ -0,0 +1,27 @@
+dic = {'c': 2, 'a': 3, 'b': 1}
+list = ['a','a','a','b','b','b']
+
+print 'print key - value pairs'
+for key in dic:
+    print key, dic[key]
+
+print 'sorted by keys'
+for key in sorted(dic):
+    print key, dic[key]
+
+print 'sorted by values'
+for key in sorted(dic, key=dic.get):# reverse=True):
+    print key, dic[key]
+
+print 'grouping (accumulating) values by keys'
+dic_accum = {}
+for element in list:
+    if element in dic_accum:
+        dic_accum[element] += 1
+    else:
+        dic_accum[element] = 1
+print dic_accum
+
+print 'add pair key-value'
+dic['d'] = 4
+print dic
